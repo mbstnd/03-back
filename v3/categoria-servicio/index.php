34febc82672eadb66c119401906f6b01f8b2038b
mbstnd <mb-quevedo@hotmail.com>
2024-06-06 05:07:49 -0400

    swagger: implementacion documentacion endpoint categoria servicio, finalizado

--- v3/categoria-servicio/index.php
+++ v3/categoria-servicio/index.php
@@ -24,110 +24,119 @@ if ($_version == 'v3') {
     if ($_mantenedor == 'categoria-servicio') {
         switch ($_metodo) {
             case 'GET':
-                if ($_header == $_token_get){ 
+                if ($_header == $_token_get) {
                     include_once 'controller.php';
                     include_once '../conexion.php';
                     $control = new Controlador();
                     $lista = $control->getAll();
                     http_response_code(200);
                     echo json_encode(["data" => $lista]);
-                }else{
+                } else {
                     http_response_code(401);
                     echo json_encode(["Error" => "No tiene autorizacion GET"]);
                 }
                 break;
-             case 'POST':
-                    if($_header == $_token_post){
+            case 'POST':
+                if ($_header == $_token_post) {
                     include_once 'controller.php';
                     include_once '../conexion.php';
                     $control = new Controlador();
                     $body = json_decode(file_get_contents("php://input", true));
                     // var_dump($body);
                     $respuesta = $control->postNuevo($body);
-                    if($respuesta){
+                    if ($respuesta) {
                         http_response_code(201);
                         echo json_encode(["data" => $respuesta]);
-                    }else{
+                    } else {
                         http_response_code(409);
                         echo json_encode(["data" => "Error: El nombre ingresado ya existe. Por favor, elija un nombre diferente."]);
-
                     }
-                    }else{
-                        http_response_code(401);
-                        echo json_encode(["Error" => "No tiene autorizacion POST"]);
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion POST"]);
+                }
+                break;
+            case 'PATCH':
+                if ($_header == $_token_patch) {
+                    if ($existeId && $existsAction) {
+                        include_once 'controller.php';
+                        include_once '../conexion.php';
+                        $control = new Controlador();
+                        if ($valueAction == 'activate') {
+                            // echo "patch...id: $valorId action:$valueAction";
+                            $respuesta = $control->patchEncenderApagar($valorId, 'true');
+                            http_response_code(200);
+                            echo json_encode(["data" => $respuesta]);
+                        } else if ($valueAction == 'desactivate') {
+                            $respuesta = $control->patchEncenderApagar($valorId, 'false');
+                            http_response_code(200);
+                            echo json_encode(["data" => $respuesta]);
+                        } else {
+                            http_response_code(400);
+                            echo json_encode(["Error" => "Los parámetros proporcionados no son los requeridos. Por favor, revise la documentación y envíe los parámetros correctos."]);
+                        }
                     }
-                    break;
-                case 'PATCH':
-                        if($_header == $_token_patch){
-                            if($existeId && $existsAction){
-                                include_once 'controller.php';
-                                include_once '../conexion.php';
-                                $control = new Controlador();
-                                if($valueAction == 'activate'){
-                                    // echo "patch...id: $valorId action:$valueAction";
-                                    $respuesta = $control->patchEncenderApagar($valorId, 'true');
-                                    http_response_code(200);
-                                    echo json_encode(["data" => $respuesta]);
-                                }else if($valueAction == 'desactivate'){
-                                    $respuesta = $control->patchEncenderApagar($valorId, 'false');
-                                    http_response_code(200);
-                                    echo json_encode(["data" => $respuesta]);
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion PATCH"]);
+                }
+                break;
+            case 'PUT':
+                if ($_header == $_token_put) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    $body = json_decode(file_get_contents("php://input", true));
+                    // var_dump($body);
+                    if (strlen($body->nombre) > 0) {
+                        $respuesta = $control->putNombreById($body->nombre, $body->id);
+                    }
+                    if (strlen($body->imagen) > 0) {
+                        $respuesta = $control->putImagenById($body->imagen, $body->id);
+                    }
+                    if (strlen($body->texto) > 0) {
+                        $respuesta = $control->putTextoById($body->texto, $body->id);
+                    }
+                    http_response_code(200);
+                    echo json_encode(["data" => $respuesta]);
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion PUT"]);
 
-                                }else{
-                                    http_response_code(400);
-                                    echo json_encode(["Error" => "Los parámetros proporcionados no son los requeridos. Por favor, revise la documentación y envíe los parámetros correctos."]);
-                                }
+                    http_response_code(406);
+                    echo json_encode(["data" => false]);
+                }
+                break;
+            case 'DELETE':
+                if ($_header == $_token_delete) {
+                    include_once 'controller.php';
+                    include_once '../conexion.php';
+                    $control = new Controlador();
+                    if ($existeId) {
+                        $listado = $control->getAll();
+                        $existeRegistro = false;
+                        foreach ($listado as $item) {
+                            if ($item['id'] == $valorId) {
+                                $existeRegistro = true;
                             }
-                        }else{
-                            http_response_code(401);
-                            echo json_encode(["Error" => "No tiene autorizacion PATCH"]);
                         }
-                        break;
-                    case 'PUT':
-                            if($_header == $_token_put){
-                                include_once 'controller.php';
-                                include_once '../conexion.php';
-                                $control = new Controlador();
-                                $body = json_decode(file_get_contents("php://input", true));
-                                // var_dump($body);
-                                if(strlen($body->nombre) >0){
-                                    $respuesta = $control->putNombreById($body->nombre, $body->id);
-
-                                }
-                                if(strlen($body->imagen) >0){
-                                    $respuesta = $control->putImagenById($body->imagen, $body->id);
-
-                                }
-                                if (strlen($body->texto) >0){
-                                    $respuesta = $control->putTextoById($body->texto, $body->id);
-                                }
-                                http_response_code(200);
-                                echo json_encode(["data" => $respuesta]);
-                            }else {
-                                http_response_code(401);
-                                echo json_encode(["Error" => "No tiene autorizacion PUT"]);
-                            }
-                            break;
-                    case 'DELETE':
-                        if($_header == $_token_delete){
-                                include_once 'controller.php';
-                                include_once '../conexion.php';
-                                $control = new Controlador();
-                                if($existeId){
-                                    $respuesta = $control->deleteById($valorId);
-                                    http_response_code(200);
-                                    echo json_encode(["data" => $respuesta]);
-                                }else {
-                                    http_response_code(400);
-                                    echo json_encode(["Error" => "Los parámetros proporcionados no son los requeridos. Por favor, revise la documentación y envíe los parámetros correctos."]);
-                                }
-
-                        }else{
-                            http_response_code(401);
-                            echo json_encode(["Error" => "No tiene autorizacion DELETE"]);
-
+                        if (!$existeRegistro) {
+                            http_response_code(404);
+                            echo json_encode(["data" => false]);
+                        } else {
+                            $respuesta = $control->deleteById($valorId);
+                            http_response_code(200);
+                            echo json_encode(["data" => $respuesta]);
                         }
-                            break;        
+                    } else {
+                        http_response_code(406);
+                        echo json_encode(["data" => false]);
+                    }
+                } else {
+                    http_response_code(401);
+                    echo json_encode(["Error" => "No tiene autorizacion DELETE"]);
+                }
+                break;
             default:
                 http_response_code(405);
                 echo json_encode(["Error" => "No implementado"]);
